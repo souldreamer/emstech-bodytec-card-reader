71360039b4f56a87bc6a53275202facf6f7d121d
Ionut Costica <ionut.costica@gmail.com>
2016-08-01 16:14:56 +0300

    fix(app): read & write functionality restored

--- app.ts
+++ app.ts
@@ -13,29 +13,20 @@ mifare.onCard((card: mifare.Card) => {
 		.then((uid: Buffer) => {
 			let information: Buffer = new Buffer([]);
 			let promise: Promise<any> = card.loadAuthKey(0, Buffer.concat([uid, new Buffer([0x4D, 0x42])]));
-			for (let block = 0; block < 0x3B; block++) {
-				if ((block + 1) % 4 === 0) continue;
-				promise = promise
-					.then(_ => card.authenticate(block, mifare.KeyType.B, 0))
-					.catch(err => console.error(`Authentication error: ${err}`))
-					.then(_ => card.readBlock(block, 0x10))
-					.then((data: Buffer) => {
-						information = Buffer.concat([information, data]);
-						console.log(block.toString(16), data)
-					})
-					.catch(err => console.error(`Data read error: ${err}`));
-			}*/
-			//promise = promise.then(_ => card.loadAuthKey(1, new Buffer([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])));
 			let step = 1;
 			for (let block = 0; block < 0x3C; block += step) {
+				if ((block+1) %4 === 0) continue;
 				promise = promise
-					.then(_ => card.authenticate(block, mifare.KeyType.B, keyLocation))
+					.then(_ => card.authenticate(block, mifare.KeyType.B, 0))
 					.catch(err => {
 						console.error(`Authentication error: ${err}`);
 						return card.authenticate(block, mifare.KeyType.B, 1);
 					})
-					.then(_ => card.readBlock(block, step << 4))
-					.then(data => console.log(data.toString('hex')))
+					.then(_ => card.readBlock(block))
+					.then(data => {
+						information = Buffer.concat([information, data]);
+						console.log(data.toString('hex'));
+					})
 					.catch(err => console.error(err.message));
 			}
 			promise = promise
